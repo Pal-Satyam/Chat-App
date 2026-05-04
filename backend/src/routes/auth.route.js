e7ad5e779f08d4f6f79bbc5fa9c2fa31de1c154e
Pal-Satyam <starpal4877@gmail.com>
2026-05-04 12:58:45 +0530

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 const router = express.Router();
+
+router.use(arcjetProtection); // Apply Arcjet protection to all auth routes
 
 router.post("/signup", signup);
 
-router.post("/login", login);
+router.post("/login",arcjetProtection, login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile);
 
