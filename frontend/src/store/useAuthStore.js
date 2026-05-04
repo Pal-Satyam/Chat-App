bf897e1136877531ef726845da1208d22847ae9c
Pal-Satyam <starpal4877@gmail.com>
2026-05-04 20:34:47 +0530

    frontend codebase setup

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,13 @@
+import { create} from 'zustand'
+
+export const useAuthStore= create((set) =>({
+    authuser: {name: "john", _id: 123, age:25},
+    isLoggedIn: false,
+    
+
+    login: () =>{
+        console.log("We just logged in");
+        set({ isLoggedIn: true});
+        
+    }
+}));
