66a944f641542a0aef89d8eeabff779ce565a068
Pal-Satyam <starpal4877@gmail.com>
2026-05-19 10:04:13 +0530

    refactor signup controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -39,8 +39,8 @@ export const signup = async (req, res) => {
 
     if (newUser) {
       
-      generateToken(newUser._id, res);
-      await newUser.save();
+      // generateToken(newUser._id, res);
+      // await newUser.save();
 
       
       const savedUser = await newUser.save();
